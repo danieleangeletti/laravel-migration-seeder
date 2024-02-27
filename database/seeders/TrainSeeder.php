78df699cd1a41c252a1030f3ddde08e0a7ca7b4d
danieleangeletti <angelettidaniele@gmail.com>
2024-02-27 21:27:13 +0100

    milestone 2 completata

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -18,16 +18,33 @@ class TrainSeeder extends Seeder
         Train::truncate();
 
         $today = date("Y-m-d");
-        $train = new Train();
-        $train->company = "Deutsche Bahn";
-        $train->date = $today;
-        $train->departure_station = "Innsbruck";
-        $train->arrival_station = "Kitzbühel";
-        $train->departure_time = "18:15:00";
-        $train->arrival_time = "19:15:00";
-        $train->train_code = "19022";
-        $train->in_time = true;
-        $train->cancelled = false;
-        $train->save();
+
+        for ($i = 0; $i < 15; $i++) {
+            $train = new Train();
+            $train->company = fake()->words(2, true);
+            $train->date = $today;
+            $train->departure_station = fake()->words(1, true);
+            $train->arrival_station = fake()->words(1, true);
+            $train->departure_time = fake()->time();
+            $train->arrival_time = fake()->time();
+            $train->train_code = fake()->words(1, true);
+            $train->in_time = fake()->numberBetween(0, 1);
+            $train->cancelled = fake()->numberBetween(0, 1);
+            $train->save();
+        }
+
+        for ($i = 0; $i < 35; $i++) {
+            $train = new Train();
+            $train->company = fake()->words(2, true);
+            $train->date = fake()->date;
+            $train->departure_station = fake()->words(1, true);
+            $train->arrival_station = fake()->words(1, true);
+            $train->departure_time = fake()->time();
+            $train->arrival_time = fake()->time();
+            $train->train_code = fake()->words(1, true);
+            $train->in_time = fake()->numberBetween(0, 1);
+            $train->cancelled = fake()->numberBetween(0, 1);
+            $train->save();
+        }
     }
 }
